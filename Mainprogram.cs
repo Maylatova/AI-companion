126d9339495d31cd2c9f50b793aa72916ae98416
DanDytiuk <dandytiuk@gmail.com>
2025-04-28 19:05:20 +0300

    Add files via upload

---
+++ Mainprogram.cs
@@ -0,0 +1,17 @@
+﻿using AICompanion;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Список мікрофонів ===");
+        MicrophoneManager.ListMicroDevices();
+
+        Console.WriteLine("Введіть індекс мікрофона для вибору:");
+        int index = int.Parse(Console.ReadLine());
+
+        string deviceName = MicrophoneManager.GetMicroNameByIndex(index);
+        Console.WriteLine($"Вибрано пристрій: {deviceName}");
+
+        MicrophoneWriteToCFG.SaveMicrophone(index, deviceName);
+    }
+}
---
+++ Micro.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using NAudio.Wave;
+
+namespace AICompanion
+{
+    public static class MicrophoneManager
+    {
+        public static void ListMicroDevices()
+        {
+            int waveInDevices = WaveInEvent.DeviceCount;
+            for (int i = 0; i < waveInDevices; i++)
+            {
+                var deviceInfo = WaveInEvent.GetCapabilities(i);
+                Console.WriteLine($"Індекс - {i}, Назва мікрофону: {deviceInfo.ProductName}");
+            }
+        }
+        public static string GetMicroNameByIndex(int index)
+        {
+            if (index < 0 || index >= WaveInEvent.DeviceCount)
+                throw new ArgumentOutOfRangeException(nameof(index), "Невірний індекс пристрою.");
+
+            var deviceInfo = WaveInEvent.GetCapabilities(index);
+            return deviceInfo.ProductName;
+        }
+    }
+    public static class MicrophoneWriteToCFG
+    {
+        private static readonly string cfgFilePath = "D:\\AIComp\\user.cfg";
+        public static void SaveMicrophone(int MicroID, string MicroName)
+        {
+            try
+            {
+                using StreamWriter writer = new(cfgFilePath, false);
+                writer.WriteLine($"MicroID = {MicroID}");
+                writer.WriteLine($"MicroName = {MicroName}");
+            }
+            catch (Exception er)
+            {
+                Console.WriteLine($"Виникла помилка при записі до конфігураційного файлу: {er.Message}");
+            }
+        }
+    }
+}
